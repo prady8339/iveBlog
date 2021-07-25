f49d61071ac6567a476a60333ad523ae9f20fabb
prady8339 <76277136+prady8339@users.noreply.github.com>
2021-07-25 17:20:39 +0530

    compose page:

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+
+const globalPost = [];
+
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
@@ -32,27 +35,18 @@ app.get("/compose",(req,res)=>{
 });
 
 app.post("/compose",(req,res)=>{
-  console.log(req.body.composeText);
- res.redirect("/compose");
-})
+  const post = {
+    tittle : req.body.composeTittle ,
+    text : req.body.composeText
+  };
+  globalPost.push(post);
+  res.redirect("/");
+});
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
