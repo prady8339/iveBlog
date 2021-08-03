0e578a4593bb9dcf48d5e3a4771058eb6b4565c5
prady8339 <76277136+prady8339@users.noreply.github.com>
2021-08-03 21:06:53 +0530

    complete challange without database

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 
@@ -22,7 +23,8 @@ app.use(express.static("public"));
 app.get("/",(req,res)=>{
 res.render("home",{ 
    content1 : homeStartingContent ,
-   composeText : globalPost
+   composeText : globalPost 
+   
    });
 
 });
@@ -49,12 +51,23 @@ app.post("/compose",(req,res)=>{
   res.redirect("/");
 });
 
+app.get("/post/:postName",(req,res) =>{
+  const reqTittle = _.lowerCase(req.params.postName);
+  globalPost.forEach(element =>{
+    const storedTittle = _.lowerCase(element.title);
+    if(reqTittle==storedTittle){
+      res.render("post",{ composeText : element });
+    }
+  });
+  
+});
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.listen(3000, function() {
-  
+
   console.log("Server started on port 3000");
 });
